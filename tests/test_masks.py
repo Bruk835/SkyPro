1cab64c07212f07762fe490791cb4303b2bebec5
Bruk <chsb835@gmail.com>
2026-03-19 09:58:11 +0300

    изменения isort

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,6 @@
 import pytest
-from src.masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_mask_card(three_letters_str: str, empty_str: str) -> None:
